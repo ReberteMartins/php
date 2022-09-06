2e26a3bd5780eb0f7a0d1d87e4438d14a16979ae
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-06 16:22:11 -0300

    arrays

---
+++ arrays/exemplo02.php
@@ -0,0 +1,16 @@
+<?php
+
+$pessoas = array();
+
+array_push($pessoas, array(
+    'nome' => 'Reberte',
+    'idade'=> 22
+));
+
+array_push($pessoas, array(
+    'nome' => 'Leticia',
+    'idade'=> 20
+));
+
+print_r($pessoas);
+?>
