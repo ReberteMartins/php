1b3dca284d27c74c5300a897b6fce70fbc747a9a
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-12 17:32:39 -0300

    Problema na tela resolvido

--- PDO/exemplo02.php
+++ PDO/exemplo02.php
@@ -3,7 +3,7 @@
 //$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
 
-$stmt = $conn->prepare("INSERT INT tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
 
 $login = "Jose";
 
