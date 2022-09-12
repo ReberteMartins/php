eeb2093d211fd5bca62e6d25942153933cd4cd3f
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-12 11:27:40 -0300

    Create exemplo01.php

---
+++ mysqli/exemplo01.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "dbphp7");
+
+if($conn->connect_error){
+
+    echo "Error: ". $conn->connect_error;
+    exit;
+}
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(?,?)");
+
+$stmt->bind_param("ss", "user", "12345");
+
+$stmt->execute();
+
+?>
