38cde8815837b1984f55f6d003a096e9b22993c1
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-12 17:55:26 -0300

    PDO

--- PDO/exemplo02.php
+++ PDO/exemplo02.php
@@ -1,7 +1,7 @@
 <?php
 
-//$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
-$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
 
 $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
 
