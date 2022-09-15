caba4b45a2f0f72fedf1e9df00b382800537248f
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-15 11:57:44 -0300

    aula 65 funcionando

--- DAO/index.php
+++ DAO/index.php
@@ -1,11 +1,18 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
 
 require_once("config.php");
 
-$sql = new Sql();
+//$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+//echo json_encode($usuarios);
 
+$root = new Usuario();
+
+$root->loadById(2);
+
+echo $root;
 ?>
