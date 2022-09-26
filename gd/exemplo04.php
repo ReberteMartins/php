ffbdb2c94002935f360243a55a4d3760128f99ec
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-26 14:25:54 -0300

    aula 84

--- gd/exemplo04.php
+++ gd/exemplo04.php
@@ -1,6 +1,6 @@
 <?php
 
-//header("Content-type: image/jpeg");
+header("Content-type: image/jpeg");
 
 $file = "wallpaper.jpg";
 
@@ -9,8 +9,13 @@ $new_height = 256;
 
 list($old_width, $old_height) = getImagesize($file);
 
-$new_Image = imagecreatetruecolor($new_width, $new_height);
-
+$new_image = imagecreatetruecolor($new_width, $new_height);
 $old_image = imagecreatefromjpeg($file);
 
+imagecopyresampled($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $old_width, $old_height);
+
+imagejpeg($new_image);
+
+imagedestroy($old_image);
+imagedestroy($new_image);
 ?>
