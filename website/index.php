bb435e4967965846c793aa5fd5083b9d0a80de25
ReberteMartins <63303430+ReberteMartins@users.noreply.github.com>
2022-09-27 17:33:18 -0300

    Aula 92

--- website/index.php
+++ website/index.php
@@ -1,5 +1,14 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
 ?>
